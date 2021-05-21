e37fc54a22cf5332b563b93de01dba46206378a6
huyhnueit68 <huypq_sv@hnue.edu.vn>
2021-05-21 17:57:45 +0700

    mcreate model

--- EmotionRecognition/ImportImages/admin.py
+++ EmotionRecognition/ImportImages/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import RequestImages, RequestClient
 # Register your models here.
+
+admin.site.register(RequestClient)
+admin.site.register(RequestImages)
